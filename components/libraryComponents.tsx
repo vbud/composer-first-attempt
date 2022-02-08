01f50673e7a348efeec5362828c62cdc1d9e55a8
Valjean Clark <valjeanbud@gmail.com>
2022-02-08 10:48:01 -0800

    implement comically basic component picking

---
+++ components/libraryComponents.tsx
@@ -0,0 +1,80 @@
+import Alert from '@mui/material/Alert'
+// import Autocomplete from '@mui/material/Autocomplete'
+// import Button from '@mui/material/Button'
+// import Checkbox from '@mui/material/Checkbox'
+// import RadioGroup from '@mui/material/RadioGroup'
+// import Rating from '@mui/material/Rating'
+// import Select from '@mui/material/Select'
+// import Slider from '@mui/material/Slider'
+// import Switch from '@mui/material/Switch'
+// import TextField from '@mui/material/TextField'
+// import ToggleButton from '@mui/material/ToggleButton'
+
+export const mui = {
+  Alert: {
+    component: Alert,
+    props: {
+      children: 'Nice job!',
+    },
+  },
+  //   Autocomplete: {
+  //       component: Autocomplete,
+  //       props: {
+
+  //       }
+  //   },
+  //   Button: {
+  //       component: Button,
+  //       props: {
+
+  //       }
+  //   },
+  //   Checkbox: {
+  //       component: Checkbox,
+  //       props: {
+
+  //       }
+  //   },
+  //   RadioGroup: {
+  //       component: RadioGroup,
+  //       props: {
+
+  //       }
+  //   },
+  //   Rating: {
+  //       component: Rating,
+  //       props: {
+
+  //       }
+  //   },
+  //   Select: {
+  //       component: Select,
+  //       props: {
+
+  //       }
+  //   },
+  //   Slider: {
+  //       component: Slider,
+  //       props: {
+
+  //       }
+  //   },
+  //   Switch: {
+  //       component: Switch,
+  //       props: {
+
+  //       }
+  //   },
+  //   TextField: {
+  //       component: TextField,
+  //       props: {
+
+  //       }
+  //   },
+  //   ToggleButton: {
+  //       component: ToggleButton,
+  //       props: {
+
+  //       }
+  //   },
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,58 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import { useState } from 'react'
+import styles from 'styles/Home.module.css'
+import Select from '@mui/material/Select'
+import MenuItem from '@mui/material/MenuItem'
+
+import { mui } from 'components/libraryComponents'
+
+let componentId = -1
+const generateComponentId = () => {
+  componentId += 1
+  return componentId
+}
 
 const Home: NextPage = () => {
+  const [components, setComponents] = useState({})
+
+  console.log('components', components)
+
   return (
-    <div className={styles.container}>
+    <div className={styles.app}>
       <Head>
         <title>composition</title>
         <meta name="description" content="Design with your design system" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+      <div className={styles.header}>
+        composition
+        <Select
+          className={styles.addComponent}
+          size="small"
+          value=""
+          onChange={(event) => {
+            const componentName = event.target.value
+            setComponents({
+              ...components,
+              [generateComponentId()]: mui[componentName],
+            })
+          }}
+        >
+          {Object.keys(mui).map((componentName) => (
+            <MenuItem value={componentName} key={componentName}>
+              {componentName}
+            </MenuItem>
+          ))}
+        </Select>
+      </div>
+
       <main className={styles.main}>
-        <h1>material-ui components:</h1>
+        {Object.values(components).map((component) => {
+          const Component = component.component
+          return <Component {...component.props} />
+        })}
       </main>
     </div>
   )
