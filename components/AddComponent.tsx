64dc50bd98f30bb5c6b8afa476b878756d4441e4
Valjean Clark <valjeanbud@gmail.com>
2022-09-27 10:42:11 -0700

    refactor AddComponent out of index.tsx to its own component

---
+++ components/AddComponent.tsx
@@ -0,0 +1,94 @@
+import { nanoid } from 'nanoid'
+import Select from '@mui/material/Select'
+import MenuItem from '@mui/material/MenuItem'
+
+import {
+  ComponentConfig,
+  ComponentId,
+  RootComponentConfig,
+  SavedComponentConfig,
+  SavedComponentConfigs,
+} from 'types'
+import { drawableComponents } from 'components/libraryComponents'
+
+import styles from 'styles/AddComponent.module.css'
+
+export const AddComponent = ({
+  componentConfigs,
+  rootComponentConfig,
+  selectedComponentIds,
+  setAndSaveRootComponentConfig,
+  setAndSaveComponentConfigs,
+}: {
+  componentConfigs: SavedComponentConfigs
+  rootComponentConfig: RootComponentConfig
+  selectedComponentIds: Array<ComponentId>
+  setAndSaveRootComponentConfig: (
+    rootComponentConfig: RootComponentConfig
+  ) => void
+  setAndSaveComponentConfigs: (
+    rootComponentConfig: SavedComponentConfigs
+  ) => void
+}) => {
+  return (
+    <Select
+      className={styles.root}
+      size="small"
+      value=""
+      onChange={(event) => {
+        const componentType = event.target.value as keyof ComponentConfig
+
+        const newComponentId = nanoid()
+
+        const newComponentConfig: SavedComponentConfig = {
+          componentType,
+          config: drawableComponents[componentType].defaultConfig,
+          childComponentIds: [],
+          parentComponentId: null,
+        }
+
+        const updatedComponentConfigs = {
+          ...componentConfigs,
+          [newComponentId]: newComponentConfig,
+        }
+
+        if (
+          selectedComponentIds.length === 1 &&
+          drawableComponents[
+            componentConfigs[selectedComponentIds[0]].componentType
+          ].isLayoutComponent
+        ) {
+          updatedComponentConfigs[selectedComponentIds[0]].childComponentIds = [
+            ...updatedComponentConfigs[selectedComponentIds[0]]
+              .childComponentIds,
+            newComponentId,
+          ]
+          updatedComponentConfigs[newComponentId].parentComponentId =
+            selectedComponentIds[0]
+        } else {
+          const updatedRootComponentConfig: RootComponentConfig = {
+            ...rootComponentConfig,
+            childComponentIds: [
+              ...rootComponentConfig.childComponentIds,
+              newComponentId,
+            ],
+          }
+          setAndSaveRootComponentConfig(updatedRootComponentConfig)
+        }
+
+        setAndSaveComponentConfigs(updatedComponentConfigs)
+      }}
+    >
+      {Object.keys(drawableComponents)
+        .filter(
+          (componentName) =>
+            !drawableComponents[componentName].isLayoutComponent
+        )
+        .map((componentName) => (
+          <MenuItem value={componentName} key={componentName}>
+            {componentName}
+          </MenuItem>
+        ))}
+    </Select>
+  )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,9 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import React, { useState } from 'react'
-import { nanoid } from 'nanoid'
-import Select from '@mui/material/Select'
-import MenuItem from '@mui/material/MenuItem'
 
-import {
-  ComponentId,
-  ComponentConfig,
-  SavedComponentConfigs,
-  RootComponentConfig,
-  SavedComponentConfig,
-} from 'types'
-import { drawableComponents } from 'components/libraryComponents'
+import { ComponentId, SavedComponentConfigs, RootComponentConfig } from 'types'
+import { AddComponent } from 'components/AddComponent'
 import { ComponentBrowser } from 'components/ComponentBrowser'
 import { Canvas } from 'components/Canvas'
 import { ComponentConfigEditor } from 'components/ComponentConfigEditor'
@@ -108,67 +99,13 @@ const Home: NextPage = () => {
 
       <div className={styles.header}>
         composition
-        <Select
-          className={styles.addComponent}
-          size="small"
-          value=""
-          onChange={(event) => {
-            const componentType = event.target.value as keyof ComponentConfig
-
-            const newComponentId = nanoid()
-
-            const newComponentConfig: SavedComponentConfig = {
-              componentType,
-              config: drawableComponents[componentType].defaultConfig,
-              childComponentIds: [],
-              parentComponentId: null,
-            }
-
-            const updatedComponentConfigs = {
-              ...componentConfigs,
-              [newComponentId]: newComponentConfig,
-            }
-
-            if (
-              selectedComponentIds.length === 1 &&
-              drawableComponents[
-                componentConfigs[selectedComponentIds[0]].componentType
-              ].isLayoutComponent
-            ) {
-              updatedComponentConfigs[
-                selectedComponentIds[0]
-              ].childComponentIds = [
-                ...updatedComponentConfigs[selectedComponentIds[0]]
-                  .childComponentIds,
-                newComponentId,
-              ]
-              updatedComponentConfigs[newComponentId].parentComponentId =
-                selectedComponentIds[0]
-            } else {
-              const updatedRootComponentConfig: RootComponentConfig = {
-                ...rootComponentConfig,
-                childComponentIds: [
-                  ...rootComponentConfig.childComponentIds,
-                  newComponentId,
-                ],
-              }
-              setAndSaveRootComponentConfig(updatedRootComponentConfig)
-            }
-
-            setAndSaveComponentConfigs(updatedComponentConfigs)
-          }}
-        >
-          {Object.keys(drawableComponents)
-            .filter(
-              (componentName) =>
-                !drawableComponents[componentName].isLayoutComponent
-            )
-            .map((componentName) => (
-              <MenuItem value={componentName} key={componentName}>
-                {componentName}
-              </MenuItem>
-            ))}
-        </Select>
+        <AddComponent
+          componentConfigs={componentConfigs}
+          selectedComponentIds={selectedComponentIds}
+          rootComponentConfig={rootComponentConfig}
+          setAndSaveComponentConfigs={setAndSaveComponentConfigs}
+          setAndSaveRootComponentConfig={setAndSaveRootComponentConfig}
+        />
       </div>
 
       <ComponentBrowser
