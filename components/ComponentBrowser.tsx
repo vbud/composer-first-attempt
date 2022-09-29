858c7b23933d8a9d23722b9556409f6a0b3eadeb
Valjean Clark <valjeanbud@gmail.com>
2022-09-28 19:47:32 -0700

    ComponentBrowser keyboard navigation improvements
    - only need to be focused anywhere in the browser, rather than focused on a specific component, for key presses to work
    - handle more up/down scenarios
    - add some comments since the complexity is growing

--- components/ComponentBrowser.tsx
+++ components/ComponentBrowser.tsx
@@ -25,40 +25,69 @@ export const ComponentBrowser = ({
     document.querySelector(`.${componentClassPrefix}${componentId}`).focus()
   }
 
-  const browseUp = (index: number, parentComponentId?: ComponentId) => {
-    const parentComponentConfig =
-      parentComponentId === undefined
+  const move = (direction: 'up' | 'down') => {
+    if (selectedComponentIds.length === 0) return
+
+    // When moving up, start from the first selected component.
+    // When moving down, start from the last selected component.
+    const startingComponentId =
+      direction === 'up'
+        ? selectedComponentIds[0]
+        : selectedComponentIds[selectedComponentIds.length - 1]
+
+    const componentConfig = componentConfigs[startingComponentId]
+    const { parentComponentId } = componentConfig
+    const parentConfig =
+      parentComponentId === null
         ? rootComponentConfig
         : componentConfigs[parentComponentId]
-    if (index === 0 && parentComponentId !== undefined) {
-      selectComponent(parentComponentId)
-    } else if (index > 0) {
-      selectComponent(parentComponentConfig.childComponentIds[index - 1])
+    const index = parentConfig.childComponentIds.indexOf(startingComponentId)
+
+    if (direction === 'up') {
+      if (index === 0 && parentComponentId !== null) {
+        // If no previous siblings available, and not at the root level, move to the parent
+        selectComponent(parentComponentId)
+      } else if (index > 0) {
+        const previousSiblingId = parentConfig.childComponentIds[index - 1]
+        const previousSiblingChildIds =
+          componentConfigs[previousSiblingId].childComponentIds
+        if (previousSiblingChildIds.length > 0) {
+          // If previous sibling has children, move to its last child
+          selectComponent(
+            previousSiblingChildIds[previousSiblingChildIds.length - 1]
+          )
+        } else {
+          // Otherwise, move to the previous sibling
+          selectComponent(previousSiblingId)
+        }
+      }
+    } else if (direction === 'down') {
+      if (componentConfig.childComponentIds.length > 0) {
+        // If component has children, go down a level to the first child
+        selectComponent(componentConfig.childComponentIds[0])
+      } else if (index < parentConfig.childComponentIds.length - 1) {
+        // Otherwise, if next sibling exists, go to it
+        selectComponent(parentConfig.childComponentIds[index + 1])
+      } else if (
+        // Otherwise, if this is the last child of its siblings, not at the root level, move to the parent's next sibling
+        index === parentConfig.childComponentIds.length - 1 &&
+        parentComponentId !== null
+      ) {
+        const grandparentId =
+          componentConfigs[parentComponentId].parentComponentId
+        const grandparentConfig =
+          grandparentId === null
+            ? rootComponentConfig
+            : componentConfigs[grandparentId]
+        const parentIndex =
+          grandparentConfig.childComponentIds.indexOf(parentComponentId)
+        selectComponent(grandparentConfig.childComponentIds[parentIndex + 1])
+      }
     }
   }
 
-  const browseDown = (
-    index: number,
-    componentId: ComponentId,
-    parentComponentId?: ComponentId
-  ) => {
-    const parentComponentConfig =
-      parentComponentId === undefined
-        ? rootComponentConfig
-        : componentConfigs[parentComponentId]
-    const componentConfig = componentConfigs[componentId]
-    if (index < parentComponentConfig.childComponentIds.length - 1) {
-      selectComponent(parentComponentConfig.childComponentIds[index + 1])
-    } else if (componentConfig.childComponentIds.length > 0) {
-      selectComponent(componentConfig.childComponentIds[0])
-    }
-  }
-
-  const renderComponents = (
-    componentIds: Array<ComponentId>,
-    parentComponentId?: ComponentId
-  ) => {
-    return componentIds.map((componentId, index) => {
+  const renderComponents = (componentIds: Array<ComponentId>) => {
+    return componentIds.map((componentId) => {
       const { componentType, childComponentIds } = componentConfigs[componentId]
 
       let children
@@ -67,7 +96,7 @@ export const ComponentBrowser = ({
         Array.isArray(childComponentIds) &&
         childComponentIds.length > 0
       ) {
-        children = renderComponents(childComponentIds, componentId)
+        children = renderComponents(childComponentIds)
       }
 
       const isSelected = selectedComponentIds.includes(componentId)
@@ -85,21 +114,6 @@ export const ComponentBrowser = ({
             onClick={() => {
               selectComponent(componentId)
             }}
-            // Allows element to be focused, which in turn allows the element to capture key presses
-            tabIndex={-1}
-            onKeyDown={(event) => {
-              switch (event.code) {
-                case 'Backspace':
-                  deleteSelectedComponents()
-                  break
-                case 'ArrowUp':
-                  browseUp(index, parentComponentId)
-                  break
-                case 'ArrowDown':
-                  browseDown(index, componentId, parentComponentId)
-                  break
-              }
-            }}
           >
             {componentType}
           </div>
@@ -110,7 +124,24 @@ export const ComponentBrowser = ({
   }
 
   return (
-    <div className={styles.root}>
+    <div
+      // Allows element to be focused, which in turn allows the element to capture key presses
+      tabIndex={-1}
+      onKeyDown={(event) => {
+        switch (event.code) {
+          case 'Backspace':
+            deleteSelectedComponents()
+            break
+          case 'ArrowUp':
+            move('up')
+            break
+          case 'ArrowDown':
+            move('down')
+            break
+        }
+      }}
+      className={styles.root}
+    >
       {renderComponents(rootComponentConfig.childComponentIds)}
     </div>
   )
